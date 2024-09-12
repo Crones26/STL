f6bb251e61e57e25131fb5bec15d327c2fb25046
Devya_yi23@top-academy.ru <Devya_yi23@top-academy.ru>
2024-09-12 09:26:02 +0500

    Vector List HomeWork DONE

--- STL/main.cpp
+++ STL/main.cpp
@@ -1,7 +1,7 @@
 ﻿#include<iostream>
 #include<array>
 #include<vector>
-#include<deque>
+#include<list>
 using std::cin;
 using std::cout;
 using std::endl;
@@ -11,188 +11,161 @@ using std::endl;
 
 //#define STL_ARRAY
 //#define STL_VECTOR
-//#define STL_STACK_1
-//#define STL_QUEUE_1
-#define STL_STACK_2
-#define STL_QUEUE_2
+#define STL_LIST
 
-template<typename T>void vector_info(const std::vector<T>& vec);
-template<typename T>void stack_info(std::vector<T>& stack);
-template<typename T>void queue_info(std::deque<T>& queue);
+template<typename T> void vector_info(const std::vector<T>& vec);
+template<typename T> void list_info(const std::list<T>& lst);
+
+void insert_at_index(std::vector<int>& vec, int index, int value);  // Вставка в вектор
+void remove_at_index(std::vector<int>& vec, int index);             // Удаление из вектора
+void insert_at_index(std::list<int>& lst, int index, int value);    // Вставка в список
+void remove_at_index(std::list<int>& lst, int index);               // Удаление из списка
 
 void main()
 {
-	setlocale(LC_ALL, "");
+    setlocale(LC_ALL, "");
+    //cout << "Hello STL" << endl;
 
 #ifdef STL_ARRAY
-	const int N = 5;
-	std::array<int, N> arr = { 3, 5, 8, 13, 21 };
-	for (int i = 0; i < arr.size(); i++)
-	{
-		cout << arr[i] << tab;
-	}
-	cout << endl;
+    // array - это контейнер, который хранит данные в виде статического массива.
+    // При создании 'array', кроме типа хранимых значений, так же обязательно нужно указать
+    // количество хранимых значений.
+
+    const int N = 5;
+    std::array<int, N> arr = { 3, 5, 8, 13, 21 };
+    for (int i = 0; i < arr.size(); i++)
+    {
+        cout << arr[i] << tab;
+    }
+    cout << endl;
 
 #endif // STL_ARRAY
 
 #ifdef STL_VECTOR
-	std::vector<int> vec = { 0, 1, 1, 2, 3, 5, 8, 13, 21, 34 };
-	for (int i = 0; i < vec.size(); i++)
-	{
-		cout << vec[i] << tab;
-	}
-	cout << endl;
-	vector_info(vec);
-	vec.push_back(55);
-	for (int i = 0; i < vec.size(); i++)
-	{
-		cout << vec.at(i) << tab;
-	}
-	cout << endl;
-	vector_info(vec);
-	vec.reserve(24);    //резервирует память ДО ЗАДАННОЙ ВМЕСТИТЕЛЬНОСТИ.
-	                    //если текущая вместительность больше заданной, то ничего не происходит.
-	                    //Резевирует столько вместительности, сколько мы хотим, если хотим больше чем есть на данный момент.
-	vector_info(vec);
-	vec.shrink_to_fit();//урезает вместительность до фактического размера вектора (size)
-	                    //это можно сделать в целях экономии памяти, если мы уверены что в вектор
-	                    //больше ничего не будет добавляться
-	vector_info(vec);
-	vec.resize(8);      //задает фактический размер Вектора.
-	                    //!!! При уменьшении теряются значения в конце Вектора !!!
-	for (std::vector<int>::iterator it = vec.begin(); it != vec.end(); ++it)
-	{
-		cout << *it << tab;
-	}
-	cout << endl;
-	vector_info(vec);
-	vec.resize(14);
-	for (int i : vec)cout << i << tab; cout << endl;
-	vector_info(vec);
+    // vector - это контейнер, который хранит данные в виде динамического массива.
+    // При создании Вектора обязательно задают только тип хранимых элементов.
+    std::vector<int> vec = { 0, 1, 1, 2, 3, 5, 8, 13, 21, 34 };
+    cout << "Исходный вектор: " << endl;
+    for (int i = 0; i < vec.size(); i++)
+    {
+        cout << vec[i] << tab;
+    }
+    cout << endl;
+    vector_info(vec);
+
+    int index, value;
+    cout << "Введите индекс для вставки в вектор: ";
+    cin >> index;
+    cout << "Введите значение для вставки в вектор: ";
+    cin >> value;
+    insert_at_index(vec, index, value);
+    cout << "Вектор после вставки: " << endl;
+    for (int i : vec) cout << i << tab;
+    cout << endl;
+    vector_info(vec);
+
+    cout << "Введите индекс для удаления из вектора: ";
+    cin >> index;
+    remove_at_index(vec, index);
+    cout << "Вектор после удаления: " << endl;
+    for (int i : vec) cout << i << tab;
+    cout << endl;
+    vector_info(vec);
 #endif // STL_VECTOR
 
-#ifdef STL_STACK_1
-	// Реализация стека с использованием vector
-	cout << delimiter;
-	std::vector<int> stack = { 1, 2, 3, 4, 5 };
-	// Вывод содержимого стека
-	cout << "Стек (сверху вниз): ";
-	for (int i = stack.size() - 1; i >= 0; --i)
-	{
-		cout << stack[i] << tab;
-	}
-	cout << endl;
-	cout << "Размер стека: " << stack.size() << endl;
-	// Добавление элементов в стек
-	stack.push_back(6);
-	stack.push_back(7);
-	cout << "Стек (сверху вниз): ";
-	for (int i = stack.size() - 1; i >= 0; --i)
-	{
-		cout << stack[i] << tab;
-	}
-	cout << endl;
-	cout << "Размер стека: " << stack.size() << endl;
-	// Удаление элемента из стека
-	stack.pop_back();
-	cout << "Стек (сверху вниз): ";
-	for (int i = stack.size() - 1; i >= 0; --i)
-	{
-		cout << stack[i] << tab;
-	}
-	cout << endl;
-	cout << "Размер стека: " << stack.size() << endl;
-#endif // STL_STACK_1
+#ifdef STL_LIST
+    // list - это контейнер, который хранит данные в виде двусвязного списка.
+    std::list<int> lst = { 0, 1, 1, 2, 3, 5, 8, 13, 21, 34 };
+    cout << "Исходный список: " << endl;
+    for (int i : lst) cout << i << tab;
+    cout << endl;
+    list_info(lst);
 
-#ifdef STL_QUEUE_1
-	// Реализация очереди с использованием deque
-	cout << delimiter;
-	std::deque<int> queue = { 10, 20, 30 };
-	// Вывод содержимого очереди
-	cout << "Очередь (с начала до конца): ";
-	for (int i = 0; i < queue.size(); i++)
-	{
-		cout << queue[i] << tab;
-	}
-	cout << endl;
-	cout << "Размер очереди: " << queue.size() << endl;
-	// Добавление элементов в очередь
-	queue.push_back(40);
-	queue.push_back(50);
-	cout << "Очередь (с начала до конца): ";
-	for (int i = 0; i < queue.size(); i++)
-	{
-		cout << queue[i] << tab;
-	}
-	cout << endl;
-	cout << "Размер очереди: " << queue.size() << endl;
-	// Удаление элемента из очереди
-	queue.pop_front();
-	cout << "Очередь (с начала до конца): ";
-	for (int i = 0; i < queue.size(); i++)
-	{
-		cout << queue[i] << tab;
-	}
-	cout << endl;
-	cout << "Размер очереди: " << queue.size() << endl;
+    int index, value;
+    cout << "Введите индекс для вставки в список: ";
+    cin >> index;
+    cout << "Введите значение для вставки в список: ";
+    cin >> value;
+    insert_at_index(lst, index, value);
+    cout << "Список после вставки: " << endl;
+    for (int i : lst) cout << i << tab;
+    cout << endl;
+    list_info(lst);
 
-#endif // STL_QUEUE_1
-
-#ifdef STL_STACK_2
-	// Реализация стека с использованием vector
-	cout << delimiter;
-	std::vector<int> stack = { 1, 2, 3, 4, 5 };
-	stack_info(stack);
-	// Добавляем элементы в стек
-	stack.push_back(6);
-	stack.push_back(7);
-	stack_info(stack);
-	// Удаляем элемент из стека (LIFO)
-	stack.pop_back();
-	stack_info(stack);
-#endif // STL_STACK_2
-
-#ifdef STL_QUEUE_2
-	// Реализация очереди с использованием deque
-	cout << delimiter;
-	std::deque<int> queue = { 10, 20, 30 };
-	queue_info(queue);
-	// Добавляем элементы в очередь
-	queue.push_back(40);
-	queue.push_back(50);
-	queue_info(queue);
-	// Удаляем элемент из очереди (FIFO)
-	queue.pop_front();
-	queue_info(queue);
-#endif // STL_QUEUE_2
+    cout << "Введите индекс для удаления из списка: ";
+    cin >> index;
+    remove_at_index(lst, index);
+    cout << "Список после удаления: " << endl;
+    for (int i : lst) cout << i << tab;
+    cout << endl;
+    list_info(lst);
+#endif // STL_LIST
 
 }
 
-template<typename T>void vector_info(const std::vector<T>& vec)
+// Вывод информации о векторе
+template<typename T> void vector_info(const std::vector<T>& vec)
 {
-	cout << "Size:    " << vec.size() << endl;
-	cout << "Capacity:" << vec.capacity() << endl;
-	cout << "MaxSize: " << vec.max_size() << endl;
+    cout << "Size:    " << vec.size() << endl;
+    cout << "Capacity:" << vec.capacity() << endl;
+    cout << "MaxSize: " << vec.max_size() << endl;
 }
-
-template<typename T>void stack_info(std::vector<T>& stack)
+// Вывод информации о списке
+template<typename T> void list_info(const std::list<T>& lst)
 {
-	cout << "Стек (сверху вниз): ";
-	for (int i = stack.size() - 1; i >= 0; --i)
-	{
-		cout << stack[i] << tab;
-	}
-	cout << endl;
-	cout << "Размер стека: " << stack.size() << endl;
+    cout << "Size:    " << lst.size() << endl;
+    cout << "MaxSize: " << lst.max_size() << endl;
 }
-
-template<typename T>void queue_info(std::deque<T>& queue)
+// Вставка элемента в вектор по указанному индексу
+void insert_at_index(std::vector<int>& vec, int index, int value)
 {
-	cout << "Очередь (с начала до конца): ";
-	for (const T& elem : queue)
-	{
-		cout << elem << tab;
-	}
-	cout << endl;
-	cout << "Размер очереди: " << queue.size() << endl;
+    if (index >= 0 && index <= vec.size())
+    {
+        vec.insert(vec.begin() + index, value);
+    }
+    else
+    {
+        cout << "ОШИБКА: Некорректный индекс для вставки!" << endl;
+    }
+}
+// Удаление элемента из вектора по указанному индексу
+void remove_at_index(std::vector<int>& vec, int index)
+{
+    if (index >= 0 && index < vec.size())
+    {
+        vec.erase(vec.begin() + index);
+    }
+    else
+    {
+        cout << "ОШИБКА: Некорректный индекс для удаления!" << endl;
+    }
+}
+// Вставка элемента в список по указанному индексу
+void insert_at_index(std::list<int>& lst, int index, int value)
+{
+    if (index >= 0 && index <= lst.size())
+    {
+        std::list<int>::iterator it = lst.begin();
+        std::advance(it, index);
+        lst.insert(it, value);
+    }
+    else
+    {
+        cout << "ОШИБКА: Некорректный индекс для вставки!" << endl;
+    }
+}
+// Удаление элемента из списка по указанному индексу
+void remove_at_index(std::list<int>& lst, int index)
+{
+    if (index >= 0 && index < lst.size())
+    {
+        std::list<int>::iterator it = lst.begin();
+        std::advance(it, index);
+        lst.erase(it);
+    }
+    else
+    {
+        cout << "ОШИБКА: Некорректный индекс для удаления!" << endl;
+    }
+
 }
