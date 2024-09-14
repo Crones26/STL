30fe35ca34b5b5fd40384cd4efc3debaffdee1da
Devya_yi23@top-academy.ru <Devya_yi23@top-academy.ru>
2024-09-14 12:18:23 +0500

    HomeWork GAI DONE

--- AssociativeContainers/main.cpp
+++ AssociativeContainers/main.cpp
@@ -6,8 +6,6 @@
 using std::cin;
 using std::cout;
 using std::endl;
-using std::vector;
-using std::string;
 
 #define tab "\t"
 #define delimiter "\n-------------------------------------\n"
@@ -17,21 +15,22 @@ void main()
     setlocale(LC_ALL, "");
 
     // Используем std::map для хранения номера автомобиля и списка правонарушений
-    std::map<string, vector<string>> trafficDatabase;
+    std::map<std::string, std::vector<std::string>> trafficDatabase;
     // Добавляем квитанции (если номер автомобиля новый, создаётся новый узел)
-    auto addViolation = [](std::map<string, vector<string>>& db, const string& plate, const string& violation)
+    auto addViolation = [](std::map<std::string, std::vector<std::string>>& db, const std::string& plate, const std::string& violation)
         {
-        // Если номер автомобиля уже есть в базе, добавляем правонарушение к существующему списку
-        if (db.find(plate) != db.end())
-        {
-            db[plate].push_back(violation);
-        }
-        else
-        {
-            // Если номера нет, создаём новый узел с этим номером и добавляем правонарушение
-            db[plate] = vector<string>{ violation };
-        }
-};
+            // Если номер автомобиля уже есть в базе, добавляем правонарушение к существующему списку
+            if (db.find(plate) != db.end())
+            {
+                db[plate].push_back(violation);
+            }
+            else
+            {
+                // Если номера нет, создаём новый узел с этим номером и добавляем правонарушение
+                db[plate] = std::vector<std::string>{ violation };
+            }
+        };
+
     addViolation(trafficDatabase, "А123ВС77", "Превышение скорости");
     addViolation(trafficDatabase, "А123ВС77", "Неправильная парковка");
     addViolation(trafficDatabase, "Х987УЗ99", "Проезд на красный свет");
@@ -49,35 +48,37 @@ void main()
     addViolation(trafficDatabase, "К666РС77", "Превышение скорости");
     addViolation(trafficDatabase, "Т777УВ77", "Проезд на запрещающий сигнал светофора");
     addViolation(trafficDatabase, "Т777УВ77", "Превышение скорости");
+
     // Вывод всех автомобилей и их нарушений
-    cout << delimiter << "Вывод всех автомобилей и их правонарушений:" << endl;
+    std::cout << delimiter << "Вывод всех автомобилей и их правонарушений:" << std::endl;
     for (const auto& entry : trafficDatabase)
     {
-        cout << "Автомобиль с номером: " << entry.first << endl;
+        std::cout << "Автомобиль с номером: " << entry.first << std::endl;
         for (const auto& violation : entry.second)
         {
-            cout << tab << "Нарушение: " << violation << endl;
+            std::cout << tab << "Нарушение: " << violation << std::endl;
         }
     }
-    // Поиск и вывод информации по конкретным автомобилям
-    cout << delimiter << "Результаты поиска:" << endl;
 
-    string platesToSearch[] = { "А123ВС77", "М456ЛМ50", "Р678КХ23", "Т777УВ77", "Д222ЕФ77", "Д222ЕФ79" };
+    // Поиск и вывод информации по конкретным автомобилям
+    std::cout << delimiter << "Результаты поиска:" << std::endl;
+
+    std::string platesToSearch[] = { "А123ВС77", "М456ЛМ50", "Р678КХ23", "Т777УВ77", "Д222ЕФ77", "Д222ЕФ79" };
 
     for (const auto& plate : platesToSearch)
     {
         auto it = trafficDatabase.find(plate);
         if (it != trafficDatabase.end())
         {
-            cout << "Автомобиль с номером " << plate << " имеет следующие правонарушения:" << endl;
+            std::cout << "Автомобиль с номером " << plate << " имеет следующие правонарушения:" << std::endl;
             for (const auto& violation : it->second)
             {
-                cout << "- " << violation << endl;
+                std::cout << "- " << violation << std::endl;
             }
         }
         else
         {
-            cout << "Автомобиль с номером " << plate << " не найден." << endl;
+            std::cout << "Автомобиль с номером " << plate << " не найден." << std::endl;
         }
     }
 
