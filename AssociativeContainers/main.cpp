6b09aa4b18db1476e2b3faf4c85b20cd1db9141c
Devya_yi23@top-academy.ru <Devya_yi23@top-academy.ru>
2024-09-14 12:12:50 +0500

    HARD In Progres

--- AssociativeContainers/main.cpp
+++ AssociativeContainers/main.cpp
@@ -3,33 +3,28 @@
 #include<vector>
 #include<string>
 #include<fstream>
-#include<cstdlib>
 
 using std::cin;
 using std::cout;
 using std::endl;
-using std::vector;
-using std::string;
-using std::ifstream;
-using std::ofstream;
 
 #define tab "\t"
 #define delimiter "\n-------------------------------------\n"
 
 // Функция для загрузки данных из файла
-void loadDatabase(std::map<string, vector<string>>& db, const string& filename) {
-    ifstream file(filename);
+void loadDatabase(std::map<std::string, std::vector<std::string>>& db, const std::string& filename) {
+    std::ifstream file(filename);
     if (!file.is_open())
     {
-        cout << "Не удалось открыть файл для чтения." << endl;
+        std::cout << "Не удалось открыть файл для чтения." << std::endl;
         return;
     }
 
-    string plate, violation;
-    while (getline(file, plate))
+    std::string plate, violation;
+    while (std::getline(file, plate))
     {
-        vector<string> violations;
-        while (getline(file, violation) && violation != "---")
+        std::vector<std::string> violations;
+        while (std::getline(file, violation) && violation != "---")
         {
             violations.push_back(violation);
         }
@@ -38,45 +33,48 @@ void loadDatabase(std::map<string, vector<string>>& db, const string& filename) 
 
     file.close();
 }
+
 // Функция для сохранения данных в файл
-void saveDatabase(const std::map<string, vector<string>>& db, const string& filename) {
-    ofstream file(filename);
+void saveDatabase(const std::map<std::string, std::vector<std::string>>& db, const std::string& filename) {
+    std::ofstream file(filename);
     if (!file.is_open())
     {
-        cout << "Не удалось открыть файл для записи." << endl;
+        std::cout << "Не удалось открыть файл для записи." << std::endl;
         return;
     }
     for (const auto& entry : db)
     {
-        file << entry.first << endl;
+        file << entry.first << std::endl;
         for (const auto& violation : entry.second)
         {
-            file << violation << endl;
+            file << violation << std::endl;
         }
-        file << "---" << endl; // Разделитель для каждого автомобиля
+        file << "---" << std::endl; // Разделитель для каждого автомобиля
     }
     file.close();
 }
+
 // Функция для вывода всех данных
-void printDatabase(const std::map<string, vector<string>>& db)
+void printDatabase(const std::map<std::string, std::vector<std::string>>& db)
 {
     if (db.empty())
     {
-        cout << "База данных пуста." << endl;
+        std::cout << "База данных пуста." << std::endl;
         return;
     }
-    cout << delimiter << "Все данные в базе нарушений:" << endl;
+    std::cout << delimiter << "Все данные в базе нарушений:" << std::endl;
     for (const auto& entry : db)
     {
-        cout << "Автомобиль с номером: " << entry.first << endl;
+        std::cout << "Автомобиль с номером: " << entry.first << std::endl;
         for (const auto& violation : entry.second)
         {
-            cout << tab << "Нарушение: " << violation << endl;
+            std::cout << tab << "Нарушение: " << violation << std::endl;
         }
     }
 }
+
 // Функция для открытия файла
-void openFile(const string& filename) {
+void openFile(const std::string& filename) {
     // Открываем файл с использованием системной команды в зависимости от ОС
 #ifdef _WIN32
     system(("start " + filename).c_str());
@@ -85,23 +83,24 @@ void openFile(const string& filename) {
 #elif __linux__
     system(("xdg-open " + filename).c_str());
 #else
-    cout << "Открытие файла не поддерживается на этой ОС." << endl;
+    std::cout << "Открытие файла не поддерживается на этой ОС." << std::endl;
 #endif
 }
+
 // Функция для удаления правонарушения по номеру автомобиля
-void removeViolation(std::map<string, vector<string>>& db, const string& plate, const string& filename) {
+void removeViolation(std::map<std::string, std::vector<std::string>>& db, const std::string& plate, const std::string& filename) {
     auto it = db.find(plate);
     if (it != db.end())
     {
-        cout << "Список правонарушений для автомобиля с номером " << plate << ":" << endl;
+        std::cout << "Список правонарушений для автомобиля с номером " << plate << ":" << std::endl;
         for (size_t i = 0; i < it->second.size(); ++i)
         {
-            cout << i + 1 << ". " << it->second[i] << endl;
+            std::cout << i + 1 << ". " << it->second[i] << std::endl;
         }
 
-        cout << "Введите номер правонарушения, которое хотите удалить: ";
+        std::cout << "Введите номер правонарушения, которое хотите удалить: ";
         size_t index;
-        cin >> index;
+        std::cin >> index;
 
         if (index > 0 && index <= it->second.size())
         {
@@ -111,85 +110,85 @@ void removeViolation(std::map<string, vector<string>>& db, const string& plate, 
                 db.erase(it); // Если больше нет нарушений, удаляем запись об автомобиле
             }
             saveDatabase(db, filename); // Сохраняем изменения
-            cout << "Правонарушение удалено и база данных обновлена." << endl;
+            std::cout << "Правонарушение удалено и база данных обновлена." << std::endl;
         }
         else
         {
-            cout << "Неверный индекс. Попробуйте снова." << endl;
+            std::cout << "Неверный индекс. Попробуйте снова." << std::endl;
         }
     }
     else
     {
-        cout << "Автомобиль с номером " << plate << " не найден." << endl;
+        std::cout << "Автомобиль с номером " << plate << " не найден." << std::endl;
     }
 }
 
 int main() {
     setlocale(LC_ALL, "");
 
-    std::map<string, vector<string>> trafficDatabase;
-    string filename = "traffic_database.txt";
+    std::map<std::string, std::vector<std::string>> trafficDatabase;
+    std::string filename = "traffic_database.txt";
     loadDatabase(trafficDatabase, filename);
 
-    auto addViolation = [](std::map<string, vector<string>>& db, const string& plate, const string& violation, const string& filename)
+    auto addViolation = [](std::map<std::string, std::vector<std::string>>& db, const std::string& plate, const std::string& violation, const std::string& filename)
         {
-        if (db.find(plate) != db.end())
-        {
-            db[plate].push_back(violation);
-        }
-        else
-        {
-            db[plate] = vector<string>{ violation };
-        }
-        saveDatabase(db, filename); // Автосохранение после добавления
+            if (db.find(plate) != db.end())
+            {
+                db[plate].push_back(violation);
+            }
+            else
+            {
+                db[plate] = std::vector<std::string>{ violation };
+            }
+            saveDatabase(db, filename); // Автосохранение после добавления
         };
 
     int choice = 0;
 
     while (true)
     {
-        cout << delimiter;
-        cout << "Выберите действие:\n";
-        cout << "1. Добавить правонарушение\n";
-        cout << "2. Проверить правонарушения по номеру автомобиля\n";
-        cout << "3. Открыть и просмотреть все сохранённые данные\n";
-        cout << "4. Удалить правонарушение\n";
-        cout << "5. Открыть файл с нарушениями\n";
-        cout << "6. Сохранить данные и выйти\n";
-        cout << "Ваш выбор: ";
-        cin >> choice;
+        std::cout << delimiter;
+        std::cout << "Выберите действие:\n";
+        std::cout << "1. Добавить правонарушение\n";
+        std::cout << "2. Проверить правонарушения по номеру автомобиля\n";
+        std::cout << "3. Открыть и просмотреть все сохранённые данные\n";
+        std::cout << "4. Удалить правонарушение\n";
+        std::cout << "5. Открыть файл с нарушениями\n";
+        std::cout << "6. Сохранить данные и выйти\n";
+        std::cout << "Ваш выбор: ";
+        std::cin >> choice;
 
         if (choice == 1)
         {
-            string plate, violation;
-            cout << "Введите номер автомобиля: ";
-            cin >> plate;
+            std::string plate, violation;
+            std::cout << "Введите номер автомобиля: ";
+            std::cin >> plate;
 
-            cout << "Введите правонарушение: ";
-            cin.ignore();
-            getline(cin, violation);
+            std::cout << "Введите правонарушение: ";
+            std::cin.ignore();
+            std::getline(std::cin, violation);
 
             addViolation(trafficDatabase, plate, violation, filename);
-            cout << "Данные добавлены и сохранены!" << endl;
+            std::cout << "Данные добавлены и сохранены!" << std::endl;
         }
         else if (choice == 2)
         {
-            string searchPlate;
-            cout << "Введите номер автомобиля для поиска: ";
-            cin >> searchPlate;
+            std::string searchPlate;
+            std::cout << "Введите номер автомобиля для поиска: ";
+            std::cin >> searchPlate;
 
             auto it = trafficDatabase.find(searchPlate);
             if (it != trafficDatabase.end())
             {
-                cout << "Автомобиль с номером " << searchPlate << " имеет следующие правонарушения:" << endl;
-                for (const auto& violation : it->second) 
+                std::cout << "Автомобиль с номером " << searchPlate << " имеет следующие правонарушения:" << std::endl;
+                for (const auto& violation : it->second)
                 {
-                    cout << "- " << violation << endl;
+                    std::cout << "- " << violation << std::endl;
                 }
             }
             else
             {
-                cout << "Автомобиль с номером " << searchPlate << " не найден." << endl;
+                std::cout << "Автомобиль с номером " << searchPlate << " не найден." << std::endl;
             }
         }
         else if (choice == 3)
@@ -198,9 +197,9 @@ int main() {
         }
         else if (choice == 4)
         {
-            string plate;
-            cout << "Введите номер автомобиля, для которого нужно удалить правонарушение: ";
-            cin >> plate;
+            std::string plate;
+            std::cout << "Введите номер автомобиля, для которого нужно удалить правонарушение: ";
+            std::cin >> plate;
 
             removeViolation(trafficDatabase, plate, filename);
         }
@@ -211,12 +210,12 @@ int main() {
         else if (choice == 6)
         {
             saveDatabase(trafficDatabase, filename);
-            cout << "Данные сохранены. Выход из программы." << endl;
+            std::cout << "Данные сохранены. Выход из программы." << std::endl;
             break;
         }
         else
         {
-            cout << "Неверный выбор. Попробуйте снова." << endl;
+            std::cout << "Неверный выбор. Попробуйте снова." << std::endl;
         }
     }
 }
