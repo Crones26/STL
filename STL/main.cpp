168e5a064f2c57098734952e83932c67c96a007d
Devya_yi23@top-academy.ru <Devya_yi23@top-academy.ru>
2024-09-14 08:35:14 +0500

    Sequence containers DONE

--- STL/main.cpp
+++ STL/main.cpp
@@ -1,6 +1,8 @@
 ﻿#include <iostream>
 #include<array>
 #include<vector>
+#include<list>
+
 using std::cin;
 using std::cout;
 using std::endl;
@@ -9,7 +11,8 @@ using std::endl;
 #define delimiter "\n----------------------------------\n"
 
 //#define STL_ARRAY
-#define STL_VECTOR
+//#define STL_VECTOR
+#define STL_LIST
 
 template<typename T>void vector_info(const std::vector<T>& vec);
 
@@ -67,11 +70,70 @@ void main()
 	}
 	cout << endl;
 	vector_info(vec);
-	vec.resize(14);
+	//vec.resize(14);
 	for (int i : vec)cout << i << tab; cout << endl;
 	vector_info(vec);
+
+	vec.insert(vec.begin() + 4, { 1024, 2048, 3072, 4096 });
+	for (int i : vec)cout << i << tab; cout << endl;
+
+	int index;
+	int count;
+	int value;
+	cout << "Введите индекс добавляемого элемента: "; cin >> index;
+	cout << "Введите количество добавляемых элементов: "; cin >> count;
+	cout << "Введите значение добавляемого элемента: "; cin >> value;
+
+	if (index >= 0 && index <= vec.size())vec.insert(vec.begin() + index, count, value);
+	else cout << "Индекс вне диапазона!" << endl;
+	for (int i : vec)cout << i << tab; cout << endl;
+	vector_info(vec);
+
+	cout << "Введите индекс удаляемого элемента: "; cin >> index;
+	cout << "Введите количество удаляемых элементов: "; cin >> count;
+	if (index >= 0 && index <= vec.size())vec.erase(vec.begin() + index, vec.begin() + index + count);
+	else cout << "Индекс вне диапазона!" << endl;
+	for (int i : vec)cout << i << tab; cout << endl;
+
 #endif // STL_VECTOR
 
+#ifdef STL_LIST
+	// list - это контейнер, который хранит данные в виде двусвязного списка.
+	std::list<int> list = { 0, 1, 1, 2, 3, 5, 8, 13, 21, 34 };
+	for (int i : list)cout << i << tab; cout << endl;
+	cout << list.front() << endl;
+	cout << list.back() << endl;
+
+	int index;
+	int value;
+	cout << "Введите индекс добавляемого элемента: "; cin >> index;
+	cout << "Введите значение добавляемого элемента: "; cin >> value;
+
+	if (index >= 0 && index <= list.size())
+	{
+		std::list<int>::iterator it = list.begin();
+		std::advance(it, index);
+		list.insert(it, value);
+	}
+	else
+	{
+		cout << "Индекс вне диапазона!" << endl;
+	}
+	for (int i : list)cout << i << tab; cout << endl;
+
+	cout << "Введите индекс удаляемого элемента: "; cin >> index;
+	if (index >= 0 && index <= list.size())
+	{
+		std::list<int>::iterator it = list.begin();
+		std::advance(it, index); 
+		list.erase(it);
+	}
+	else
+	{
+		cout << "Индекс вне диапазона!" << endl;
+	}
+	for (int i : list)cout << i << tab; cout << endl;
+#endif // STL_LIST
 }
 
 template<typename T>void vector_info(const std::vector<T>& vec)
